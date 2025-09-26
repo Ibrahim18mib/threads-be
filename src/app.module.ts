8eb9d17484f059d7ab371935ff17d5a26f5649db
Ibrahim18mib <ibrahimeblakwt@gmail.com>
2025-09-26 02:44:34 -1200

    Integrated the users resources and integrated the get and post users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { CommentsModule } from './comments/comments.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    CommentsModule,
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-thread'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
