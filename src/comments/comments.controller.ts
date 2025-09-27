be5c9daffef6df04632fd471115ec994fee05482
Ibrahim18mib <ibrahimeblakwt@gmail.com>
2025-09-26 20:50:50 -1200

    Created the Comments Resource and integrated the functionality for the Comments and Comments by ParentId,
    Added the GlobalFilterPipe for the validationErrorFilter in main.ts to handle the parentid in queryparams

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+  BadRequestException,
+} from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
@@ -13,8 +23,18 @@ export class CommentsController {
   }
 
   @Get()
-  findAll() {
-    return this.commentsService.findAll();
+  findAll(@Query() queryParam: any) {
+    if (queryParam.parentId) {
+      try {
+        return this.commentsService.getCommentsByParentId(queryParam.parentId);
+      } catch (error) {
+        throw new BadRequestException('Something bad happened', {
+          cause: new Error(error.message),
+          description: 'Some error description',
+        });
+      }
+    }
+    return this.commentsService.getTopLevelComments();
   }
 
   @Get(':id')
--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,15 +1,46 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Comment } from './schemas/comment.schema';
 
 @Injectable()
 export class CommentsService {
+  constructor(
+    @InjectModel(Comment.name) private commentModel: Model<Comment>,
+  ) {}
   create(createCommentDto: CreateCommentDto) {
-    return 'This action adds a new comment';
+    const createdComment = this.commentModel.create({
+      text: createCommentDto.text,
+      user: createCommentDto.userId,
+      parent: createCommentDto.parentId || null,
+    });
+    return createdComment.then((doc) => {
+      return doc.populate(['user', 'parent']);
+    });
   }
 
   findAll() {
-    return `This action returns all comments`;
+    return this.commentModel.find().populate(['user', 'parent']).exec();
+  }
+
+  getTopLevelComments() {
+    return this.commentModel
+      .find({
+        parent: null,
+      })
+      .populate(['user', 'parent'])
+      .exec();
+  }
+
+  getCommentsByParentId(parentId: string) {
+    return this.commentModel
+      .find({
+        parent: parentId,
+      })
+      .populate(['user', 'parent'])
+      .exec();
   }
 
   findOne(id: number) {
